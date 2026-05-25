a0d5d544da1414d34ddbc007f5824adf1d865e6e
Matthew Lawrence <matthewconnor0418@gmail.com>
2026-05-25 13:30:14 +0200

    Added clickMeButton for displayTxt

--- app/src/main/java/com/example/ascii_app/MainActivity.kt
+++ app/src/main/java/com/example/ascii_app/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.ascii_app
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,7 +14,9 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        
+        //fetching the button using the id we set on the user interface//
+        val displayTxt= findViewById<TextView>(R.id.displayTxt)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
